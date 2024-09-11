61c81c4c8d9e124027e63d235305a3c8ced1c793
fbojikdfofhdfoujmvjn <shreyashv256@gmail.com>
2024-09-11 17:13:41 +0100

    added server.js

--- server.js
+++ server.js
@@ -1,2 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/bitewave', { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static('public'));
+
+// Serve the admin page
+app.get('/admin', (req, res) => {
+    res.sendFile(__dirname + '/public/admin.html');
+});
+
+// Import and use product routes
 const productRoutes = require('./routes/products');
 app.use('/api/products', productRoutes);
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
